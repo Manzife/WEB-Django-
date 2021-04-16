14cec7f3b6239cd829ef4223d459891372841a10
Manzife <51923014+Manzife@users.noreply.github.com>
2021-04-15 21:06:57 -0300

    split

---
+++ Ordenacao.py
@@ -0,0 +1,5 @@
+def ordenar(v = verduras,f = frutas, p =  panificação):
+    
+    ordem = v + f + p 
+
+    return ordem
---
+++ Usuario.py
@@ -0,0 +1,7 @@
+def sua_lista(ordem):
+    ordenar(verduras,frutas,panificação)
+    print("você tem em sua lista de compras: " "\n" + "frutas" + frutas + "\n" + "verduras" + "verduras na seguinte oredem ideal" + ordem )
+    resposta = input("digite sim para confirmar ou digite não para trocar o item, \n se for mais de um item coloque no formato 'banana, 'abacate' : ")
+    print(resposta)
+
+    return resposta
